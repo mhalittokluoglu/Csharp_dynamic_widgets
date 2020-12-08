fd2213969bbd242fce83ceb27eb4fac8c612c05f
mhalittokluoglu <74779684+mhalittokluoglu@users.noreply.github.com>
2020-12-08 16:32:31 +0300

    Add files via upload

---
+++ Dynamic Table 01/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Dynamic_Table_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.a0 = new System.Windows.Forms.TextBox();
+            this.b0 = new System.Windows.Forms.TextBox();
+            this.c0 = new System.Windows.Forms.TextBox();
+            this.d0 = new System.Windows.Forms.TextBox();
+            this.d1 = new System.Windows.Forms.TextBox();
+            this.c1 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.TextBox();
+            this.a1 = new System.Windows.Forms.TextBox();
+            this.d2 = new System.Windows.Forms.TextBox();
+            this.c2 = new System.Windows.Forms.TextBox();
+            this.b2 = new System.Windows.Forms.TextBox();
+            this.a2 = new System.Windows.Forms.TextBox();
+            this.d3 = new System.Windows.Forms.TextBox();
+            this.c3 = new System.Windows.Forms.TextBox();
+            this.b3 = new System.Windows.Forms.TextBox();
+            this.a3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // a0
+            // 
+            this.a0.Location = new System.Drawing.Point(100, 120);
+            this.a0.Name = "a0";
+            this.a0.Size = new System.Drawing.Size(100, 20);
+            this.a0.TabIndex = 0;
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(230, 120);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(100, 20);
+            this.b0.TabIndex = 1;
+            // 
+            // c0
+            // 
+            this.c0.Location = new System.Drawing.Point(360, 120);
+            this.c0.Name = "c0";
+            this.c0.Size = new System.Drawing.Size(100, 20);
+            this.c0.TabIndex = 2;
+            // 
+            // d0
+            // 
+            this.d0.Location = new System.Drawing.Point(490, 120);
+            this.d0.Name = "d0";
+            this.d0.Size = new System.Drawing.Size(100, 20);
+            this.d0.TabIndex = 3;
+            // 
+            // d1
+            // 
+            this.d1.Location = new System.Drawing.Point(490, 150);
+            this.d1.Name = "d1";
+            this.d1.Size = new System.Drawing.Size(100, 20);
+            this.d1.TabIndex = 7;
+            // 
+            // c1
+            // 
+            this.c1.Location = new System.Drawing.Point(360, 150);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(100, 20);
+            this.c1.TabIndex = 6;
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(230, 150);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 20);
+            this.b1.TabIndex = 5;
+            // 
+            // a1
+            // 
+            this.a1.Location = new System.Drawing.Point(100, 150);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(100, 20);
+            this.a1.TabIndex = 4;
+            // 
+            // d2
+            // 
+            this.d2.Location = new System.Drawing.Point(490, 180);
+            this.d2.Name = "d2";
+            this.d2.Size = new System.Drawing.Size(100, 20);
+            this.d2.TabIndex = 11;
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(360, 180);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(100, 20);
+            this.c2.TabIndex = 10;
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(230, 180);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 20);
+            this.b2.TabIndex = 9;
+            // 
+            // a2
+            // 
+            this.a2.Location = new System.Drawing.Point(100, 180);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(100, 20);
+            this.a2.TabIndex = 8;
+            // 
+            // d3
+            // 
+            this.d3.Location = new System.Drawing.Point(490, 210);
+            this.d3.Name = "d3";
+            this.d3.Size = new System.Drawing.Size(100, 20);
+            this.d3.TabIndex = 15;
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(360, 210);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(100, 20);
+            this.c3.TabIndex = 14;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(230, 210);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 20);
+            this.b3.TabIndex = 13;
+            // 
+            // a3
+            // 
+            this.a3.Location = new System.Drawing.Point(100, 210);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(100, 20);
+            this.a3.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Change";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Change";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(631, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Change";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.d3);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.d2);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.d1);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.d0);
+            this.Controls.Add(this.c0);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.a0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox a0;
+        private System.Windows.Forms.TextBox b0;
+        private System.Windows.Forms.TextBox c0;
+        private System.Windows.Forms.TextBox d0;
+        private System.Windows.Forms.TextBox d1;
+        private System.Windows.Forms.TextBox c1;
+        private System.Windows.Forms.TextBox b1;
+        private System.Windows.Forms.TextBox a1;
+        private System.Windows.Forms.TextBox d2;
+        private System.Windows.Forms.TextBox c2;
+        private System.Windows.Forms.TextBox b2;
+        private System.Windows.Forms.TextBox a2;
+        private System.Windows.Forms.TextBox d3;
+        private System.Windows.Forms.TextBox c3;
+        private System.Windows.Forms.TextBox b3;
+        private System.Windows.Forms.TextBox a3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Dynamic Table 01/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dynamic_Table_01
+{
+    public partial class Form1 : Form
+    {
+        public static int xa, xb, xc, xd;
+        public static int[] y = new int[4];
+        public static int[] ya = new int[4];
+        public static int[] yb = new int[4];
+        public static int[] pos = new int[4];
+        public static int i;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 10;
+            timer1.Start();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a0.SetBounds(xa, y[0], 100, 20);
+            b0.SetBounds(xb, y[0], 100, 20);
+            c0.SetBounds(xc, y[0], 100, 20);
+            d0.SetBounds(xd, y[0], 100, 20);
+            a1.SetBounds(xa, y[1], 100, 20);
+            b1.SetBounds(xb, y[1], 100, 20);
+            c1.SetBounds(xc, y[1], 100, 20);
+            d1.SetBounds(xd, y[1], 100, 20);
+            a2.SetBounds(xa, y[2], 100, 20);
+            b2.SetBounds(xb, y[2], 100, 20);
+            c2.SetBounds(xc, y[2], 100, 20);
+            d2.SetBounds(xd, y[2], 100, 20);
+            a3.SetBounds(xa, y[3], 100, 20);
+            b3.SetBounds(xb, y[3], 100, 20);
+            c3.SetBounds(xc, y[3], 100, 20);
+            d3.SetBounds(xd, y[3], 100, 20);
+            for (i = 0; i < 4; i++)
+            {
+                if (y[i] < yb[i])
+                {
+                    y[i]++;
+                }
+                if (y[i] > yb[i])
+                {
+                    y[i]--;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            change(pos[0], pos[1], 0, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            change(pos[1], pos[2], 1, 2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            change(pos[2], pos[3], 2, 3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a0.Text = "1st row";
+            b0.Text = "1st row";
+            c0.Text = "1st row";
+            d0.Text = "1st row";
+            a1.Text = "2nd row";
+            b1.Text = "2nd row";
+            c1.Text = "2nd row";
+            d1.Text = "2nd row";
+            a2.Text = "3rd row";
+            b2.Text = "3rd row";
+            c2.Text = "3rd row";
+            d2.Text = "3rd row";
+            a3.Text = "4th row";
+            b3.Text = "4th row";
+            c3.Text = "4th row";
+            d3.Text = "4th row";
+            xa = 100;
+            xb = 230;
+            xc = 360;
+            xd = 490;
+            y[0] = 120;
+            y[1] = 150;
+            y[2] = 180;
+            y[3] = 210;
+            ya[0] = 120;
+            ya[1] = 150;
+            ya[2] = 180;
+            ya[3] = 210;
+            yb[0] = 120;
+            yb[1] = 150;
+            yb[2] = 180;
+            yb[3] = 210;
+            pos[0] = 0;
+            pos[1] = 1;
+            pos[2] = 2;
+            pos[3] = 3;
+        }
+        private void flippos(int j, int k)
+        {
+            int pass;
+            pass = pos[j];
+            pos[j] = pos[k];
+            pos[k] = pass;
+        }
+        private void change(int j, int k, int l, int m)
+        {
+            yb[j] = ya[m];
+            yb[k] = ya[l];
+            flippos(l, m);
+        }
+    }
+}
